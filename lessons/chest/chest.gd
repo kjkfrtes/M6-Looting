1c63717b527ad6096ae651faa71ff0364476220f
kjkfrtes <islee2@s.sfusd.edu>
2026-02-05 11:41:53 -0800

    M6L7

--- lessons/chest/chest.gd
+++ lessons/chest/chest.gd
@@ -4,16 +4,28 @@ extends Area2D
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 @export var possible_items: Array[PackedScene] = []
 
-
-
 func _spawn_random_item() -> void: 
+	const FLIGHT_TIME := 0.4
+	const HALF_FLIGHT_TIME := FLIGHT_TIME / 2.0 
 	var loot_item: Area2D = possible_items.pick_random().instantiate()
 	add_child(loot_item)
 	var random_angle := randf_range(0.0, 2.0 * PI)
 	var random_direction := Vector2(1.0, 0.0).rotated(random_angle)
 	var random_distance := randf_range(60.0,120.0)
-	loot_item.position = random_direction * random_distance
-
+	var land_position = random_direction * random_distance
+	var tween := create_tween()
+	loot_item.scale = Vector2(0.25,0.25)
+	tween.set_parallel()
+	tween.tween_property(loot_item, "scale", Vector2(1.0,1.0) ,HALF_FLIGHT_TIME)
+	tween.tween_property(loot_item, "position:x", land_position.x, FLIGHT_TIME)
+	tween = create_tween()
+	tween.set_trans(Tween.TRANS_QUAD)
+	tween.set_ease(Tween.EASE_OUT)
+	var jump_height := randf_range(30.0, 80.0)
+	tween.tween_property(loot_item, "position:y" , land_position.y - jump_height, HALF_FLIGHT_TIME)
+	tween.set_ease(Tween.EASE_OUT)
+	tween.tween_property(loot_item, "position:y" , land_position.y , HALF_FLIGHT_TIME)
+	
 func open()-> void:
 	animation_player.play("open")
 	input_pickable = false
